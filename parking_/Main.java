28baed2bf8fb14445474d8aeca29f683616b52d4
Leticia Viana <leticiapaula1754@gmail.com>
2024-02-28 12:27:28 -0300

    adiconado classe ao package parking

---
+++ parking_/Main.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner questions = new Scanner(System.in);
+        DataCar DataCar = new DataCar("", "", "", "");
+
+        List<DataCar> list = new LinkedList<>();
+
+
+
+
+        questions.close();
+    }
+}
